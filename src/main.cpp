cdd1412e04fbcb4509d3fae3de10d7bed7d01bc4
Subodh Singh <singh.01.subodh@gmail.com>
2025-03-21 18:59:06 +0530

    Some stuff on screen
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,71 @@
 #include "main.h"
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 #include <stdio.h>
+#include <random>
+#include <vector>
 #include "ecs/ECS.h"
 
 static void handle_events(sf::RenderWindow &window);
 
-struct Position2D {
-    float y;
-    float x;
-};
+
+const double LENGTH = 1.0;
+const double RADIUS = 0.02;
+
+static std::mt19937 gen(std::random_device{}());
+static std::uniform_real_distribution<> posDis(RADIUS, LENGTH - RADIUS);
+static std::uniform_real_distribution<> velDis(RADIUS, LENGTH - RADIUS);
+
+
+static ECS::EntityManager entityManager{};
+static ECS::ComponentManager<Position2D> positions;
+static ECS::ComponentManager<Velocity2D> velocities;
+
+static std::vector<ECS::Entity> discs;
+
+static void init_entities(uint32_t count)
+{
+    while (count-- > 0)
+    {
+        ECS::Entity disc = entityManager.createEntity();
+        discs.push_back(disc);
+        positions.addComponent(disc, {posDis(gen), posDis(gen)});
+        velocities.addComponent(disc, {velDis(gen), velDis(gen)});
+    }
+}
+
+static void draw(sf::RenderWindow &window)
+{
+    float radius = RADIUS * WIDTH;
+    sf::CircleShape circle(radius);
+    circle.setFillColor(sf::Color(0, 255, 255));
+    for (auto entity: discs)
+    {
+        Position2D *pos = positions.getComponent(entity);
+        float y = pos->y * HEIGHT;
+        y -= radius;
+        float x = pos->x * WIDTH;
+        x -= radius;
+        circle.setPosition({x, y});
+        window.draw(circle);
+    }
+}
 
 int main()
 {
-    // test ECS
-    ECS::EntityManager entityManager{};
-    ECS::ComponentManager<Position2D> positionComponents{};
-    ECS::Entity one = entityManager.createEntity();
-    positionComponents.addComponent(one, {1.0, 2.0});
-    Position2D *pos = positionComponents.getComponent(one);
-    if (pos != nullptr) printf("Position of one: [%f, %f]\n", pos->y, pos->x);
-    else printf("Something is wrong with the ECS\n");
-
-    auto window = sf::RenderWindow(sf::VideoMode({WIDTH, HEIGHT}), "SFML For Fun", STYLE);
+    init_entities(100);
+    printf("Total Discs: %zu\n", discs.size());
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+    auto window = sf::RenderWindow(sf::VideoMode({WIDTH, HEIGHT}), "SFML For Fun", STYLE, sf::State::Windowed, settings);
     window.setFramerateLimit(144);
 
     while (window.isOpen())
     {
         handle_events(window);
         window.clear();
+        draw(window);
         window.display();
     }
 }
--- src/main.h
+++ src/main.h
@@ -7,3 +7,19 @@ const unsigned int HEIGHT = 600;
 const unsigned int WIDTH  = 800;
 
 const uint32_t STYLE = sf::Style::Titlebar | sf::Style::Close;
+
+// A few balls, bouncing around. Each will have a random position
+// and random velocity. They will be confined in a 1.0 x 1.0 rect
+// and when they hit a wall they there will be elastic collission.
+
+struct Position2D {
+    double y;
+    double x;
+};
+
+struct Velocity2D {
+    double vy;
+    double vx;
+};
+
+
