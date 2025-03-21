867a902c8c479d6054bf86bc5e55b69318c1b733
Subodh Singh <singh.01.subodh@gmail.com>
2025-03-21 15:15:54 +0530

    Fixed size window
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
+#include "main.h"
+
 #include <SFML/Graphics.hpp>
+#include <stdio.h>
+
+static void handle_events(sf::RenderWindow &window);
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({WIDTH, HEIGHT}), "SFML For Fun", STYLE);
     window.setFramerateLimit(144);
 
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
+        handle_events(window);
         window.clear();
         window.display();
+    }
+}
+
+static void handle_events(sf::RenderWindow &window)
+{
+
+    while (const std::optional event = window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>())
+        {
+            window.close();
+        }
+        if (event->is<sf::Event::Resized>())
+        {
+            auto size = window.getSize();
+            printf("Resized: [%u, %u]\n", size.y, size.x);
+        }
     }
 }
---
+++ src/main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <stdint.h>
+
+const unsigned int HEIGHT = 600;
+const unsigned int WIDTH  = 800;
+
+const uint32_t STYLE = sf::Style::Titlebar | sf::Style::Close;
