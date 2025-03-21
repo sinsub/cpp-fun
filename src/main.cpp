abaeeb9384036b66264c74ac258b8353a422510a
Subodh Singh <singh.01.subodh@gmail.com>
2025-03-21 16:23:16 +0530

    Add Simple and Slow ECS
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,26 @@
 
 #include <SFML/Graphics.hpp>
 #include <stdio.h>
+#include "ecs/ECS.h"
 
 static void handle_events(sf::RenderWindow &window);
 
+struct Position2D {
+    float y;
+    float x;
+};
+
 int main()
 {
+    // test ECS
+    ECS::EntityManager entityManager{};
+    ECS::ComponentManager<Position2D> positionComponents{};
+    ECS::Entity one = entityManager.createEntity();
+    positionComponents.addComponent(one, {1.0, 2.0});
+    Position2D *pos = positionComponents.getComponent(one);
+    if (pos != nullptr) printf("Position of one: [%f, %f]\n", pos->y, pos->x);
+    else printf("Something is wrong with the ECS\n");
+
     auto window = sf::RenderWindow(sf::VideoMode({WIDTH, HEIGHT}), "SFML For Fun", STYLE);
     window.setFramerateLimit(144);
 
