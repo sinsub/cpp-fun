3d814fe54899aed603000dd7460beb2146a63a99
Subodh Singh <singh.01.subodh@gmail.com>
2025-04-30 02:03:27 +0530

    Add a simple ComponentManager
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- ecs-example/src/main.cpp
+++ ecs-example/src/main.cpp
@@ -2,14 +2,40 @@
 
 #include "ecs.h"
 
+struct Position {
+    int x, y;
+};
+
 int main() {
     printf("ECS version %s\n", ecs::GetVersion());
     ecs::EntityManager em{};
+    ecs::ComponentManager<Position> cm{};
+
     ecs::Entity one = em.create();
     ecs::Entity two = em.create();
     ecs::Entity three = em.create();
-    em.erase(two);
+
+    cm.addComponent(one, {1, 1});
+    cm.addComponent(three, {3, 3});
+
     for (auto entity : em) {
-        printf("Entity: %lu\n", entity);
+        Position *pos = cm.getComponent(entity);
+        if (pos)
+            printf("Entity: %lu - Position{%d, %d}\n", entity, pos->x, pos->y);
+        else
+            printf("Entity: %lu - No Position Component\n", entity);
+    }
+
+    // TODO: this should remove components from ComponentManager
+    em.erase(three);
+
+    printf("Remove three\n");
+
+    for (auto entity : em) {
+        Position *pos = cm.getComponent(entity);
+        if (pos)
+            printf("Entity: %lu - Position{%d, %d}\n", entity, pos->x, pos->y);
+        else
+            printf("Entity: %lu - No Position Component\n", entity);
     }
 }
--- ecs/include/ecs.h
+++ ecs/include/ecs.h
@@ -1,6 +1,7 @@
 #ifndef ECS_ECS_H_
 #define ECS_ECS_H_
 
+#include "component.h"
 #include "entity.h"
 
 namespace ecs {
