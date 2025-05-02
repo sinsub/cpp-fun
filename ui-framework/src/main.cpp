a2ce8e256084408f95dd29740b8c5eba2c58ebe2
Subodh Singh <singh.01.subodh@gmail.com>
2025-05-03 00:33:38 +0530

    Add a RenderWindow
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- ui-framework/src/main.cpp
+++ ui-framework/src/main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Test UI Framework");
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) window.close();
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
 }
