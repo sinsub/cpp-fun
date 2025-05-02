9fe3c423962b588e66acd96f050b897a1340d0b8
Subodh Singh <singh.01.subodh@gmail.com>
2025-05-03 01:54:14 +0530

    Draw circle on mouse pointer
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- ui-framework/src/main.cpp
+++ ui-framework/src/main.cpp
@@ -1,13 +1,42 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Test UI Framework");
-    while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) window.close();
+bool inWindow = false;
+
+void processEvents(sf::RenderWindow& window) {
+    while (const std::optional event = window.pollEvent()) {
+        if (event->is<sf::Event::Closed>()) {
+            window.close();
+        } else if (event->is<sf::Event::MouseEntered>()) {
+            inWindow = true;
+        } else if (event->is<sf::Event::MouseLeft>()) {
+            inWindow = false;
         }
-        window.clear(sf::Color::Black);
+    }
+}
+
+void render(sf::RenderWindow& window) {
+    window.clear(sf::Color::Black);
+    if (inWindow) {
+        auto pos = sf::Mouse::getPosition(window);
+        float radius = 10.f;
+        float x = pos.x - radius;
+        float y = pos.y - radius;
+        sf::CircleShape dot(radius);
+        dot.setFillColor(sf::Color::White);
+        dot.setPosition({x, y});
+        window.draw(dot);
+    }
+}
+
+int main() {
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Test UI Framework", sf::Style::Default,
+                            sf::State::Windowed, settings);
+    while (window.isOpen()) {
+        processEvents(window);
+        render(window);
         window.display();
     }
 }
